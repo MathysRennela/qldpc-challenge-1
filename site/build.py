06d85572087d3041b62eb84c2e8673cea3d43956
Vincent Russo <vincentrusso1@gmail.com>
2026-06-17 12:24:37 -0400

    Footer: drop the duplicate 'marks the (n,k,d) Pareto frontier' (legend already says it)

--- site/build.py
+++ site/build.py
@@ -892,9 +892,8 @@ def build():
              f'<a href="{REPO}/CONTRIBUTING.md">CONTRIBUTING</a>, '
              f'<a href="{REPO}/schema/SCHEMA.md">the schema</a>, '
              f'<a href="{REPO}/TRACKS.md">the tracks</a>, and '
-             '<a href="references.html">references</a>. &#9733; marks the '
-             '(n,k,d) Pareto frontier. The site and challenge are open source '
-             f'on <a href="{REPO_ROOT}">GitHub</a>. Developed at '
+             '<a href="references.html">references</a>. The site and challenge '
+             f'are open source on <a href="{REPO_ROOT}">GitHub</a>. Developed at '
              '<a href="https://unitary.foundation">Unitary Foundation</a>. '
              f'Code under <a href="{REPO}/LICENSE">Apache 2.0</a>.</footer>')
     P.append('</div><div id=tip></div>')
